69cceec580e6cebd5d632212509cc25cb658074e
bereket-7 <bekhonelign@gmail.com>
2025-03-31 04:20:49 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import { createPinia } from "pinia"; // Import createPinia from pinia
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+
+// Create and use Pinia store
+const pinia = createPinia();
+app.use(pinia); // Use Pinia here
+
+app.use(router).mount("#app");
