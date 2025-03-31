4b2bc9f69f7f62e003fa6d235e7aa5199f626ad4
bereket-7 <bekhonelign@gmail.com>
2025-03-31 22:11:07 +0300

    initial header comp

---
+++ src/components/Layout/FanaHeader.vue
@@ -0,0 +1,9 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <v-app-bar app color="primary">
+    <v-toolbar-title>FanaMind</v-toolbar-title>
+    <v-spacer />
+    <v-btn text to="/">Home</v-btn>
+    <v-btn text to="/login">Login</v-btn>
+  </v-app-bar>
+</template>
