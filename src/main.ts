6e102f5465fe194b97e159546860263dd5383bbe
bereket-7 <bekhonelign@gmail.com>
2025-03-31 21:04:10 +0300

    vuetify install

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { createPinia } from "pinia"; // Import createPinia from pinia
+import { createPinia } from "pinia";
+import "vuetify/styles"; // Import createPinia from pinia
+import vuetify from "./plugins/vuetify"; // import Vuetify setup
 
 const app = createApp(App);
 
 // Create and use Pinia store
 const pinia = createPinia();
 app.use(pinia); // Use Pinia here
+app.use(vuetify); // use Vuetify
 
 app.use(router).mount("#app");
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import "vuetify/styles"; 
+import { createVuetify } from "vuetify";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+});
+
+export default vuetify;
